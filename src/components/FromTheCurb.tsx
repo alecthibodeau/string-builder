5c336bcd8e17dad903e596c6d9af01027eca340d
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-04-16 17:05:44 -0400

    Add FromTheCurb component and images

---
+++ src/components/FromTheCurb.tsx
@@ -0,0 +1,22 @@
+/* Constants */
+import constants from '../constants';
+
+function FromTheCurb(): JSX.Element {
+  function renderImage(imageSource: string, index: number): JSX.Element {
+    return (
+      <img
+        key={`FromTheCurbPrint${index + 1}`}
+        alt={`From the Curb print #${index + 1}`}
+        src={imageSource}
+      />
+    );
+  }
+
+  return (
+    <div>
+      {constants.images.map(renderImage)}
+    </div>
+  );
+}
+
+export default FromTheCurb;
---
+++ src/constants.ts
@@ -0,0 +1,49 @@
+import print01 from './assets/images/01pmax.gif';
+import print02 from './assets/images/02pmax.gif';
+import print03 from './assets/images/03pmax.gif';
+import print04 from './assets/images/04pmax.gif';
+import print05 from './assets/images/05pmax.gif';
+import print06 from './assets/images/06pmax.gif';
+import print07 from './assets/images/07pmax.gif';
+import print08 from './assets/images/08pmax.gif';
+import print09 from './assets/images/09pmax.gif';
+import print10 from './assets/images/10pmax.gif';
+import print11 from './assets/images/11pmax.gif';
+import print12 from './assets/images/12pmax.gif';
+import print13 from './assets/images/13pmax.gif';
+import print14 from './assets/images/14pmax.gif';
+import print15 from './assets/images/15pmax.gif';
+import print16 from './assets/images/16pmax.gif';
+import print17 from './assets/images/17pmax.gif';
+import print18 from './assets/images/18pmax.gif';
+import print19 from './assets/images/19pmax.gif';
+import print20 from './assets/images/20pmax.gif';
+
+const images: string[] = [
+  print01,
+  print02,
+  print03,
+  print04,
+  print05,
+  print06,
+  print07,
+  print08,
+  print09,
+  print10,
+  print11,
+  print12,
+  print13,
+  print14,
+  print15,
+  print16,
+  print17,
+  print18,
+  print19,
+  print20
+];
+
+const constants = {
+  images
+};
+
+export default constants;
